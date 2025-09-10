575b9f21d1ede84f84df8ad6a42fa3e15cb8b10c
Luis Robles <luisalfredorobles09@gmail.com>
2025-09-10 05:03:08 -0300

    feat(routes): se agrega crud de emapanadas

--- backend/src/routes/empanadas.js
+++ backend/src/routes/empanadas.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getAllEmpanadas } from '../controllers/empanadasController.js';
+import { getAllEmpanadas, createEmpanada, updateEmpanada, deleteEmpanada } from '../controllers/empanadasController.js';
 
 export const router = express.Router();
 
 router.get('/empanadas', getAllEmpanadas);
-
-
+router.post('/empanada', createEmpanada);
+router.put('/empanada/:id', updateEmpanada);
+router.delete('/empanada/:id', deleteEmpanada);
