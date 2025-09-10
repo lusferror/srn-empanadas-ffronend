db3d98436beb2cf578a1b6de6dda41f96ad075c1
Luis Robles <luisalfredorobles09@gmail.com>
2025-09-10 05:35:52 -0300

    feat(controllers): prueba de consulta de api

--- frontend/app/Controllers/Home.php
+++ frontend/app/Controllers/Home.php
@@ -2,10 +2,21 @@
 
 namespace App\Controllers;
 
+use Config\Services;
+
 class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        $username = env('BASIC_AUTH_USER');
+        $password = env('BASIC_AUTH_PASSWORD');
+        $client = Services::curlrequest();
+        $response = $client->get('backend:3000/api/empanadas', [
+            'headers' => [
+                'Authorization' => 'Basic ' . base64_encode("$username:$password")
+            ]
+        ]);
+        $data = json_decode($response->getBody(), true);
+        return view('home', ['empanadas' => $data['empanadas']]);
     }
 }
